62cdd735646b3dae00ca82849534f32820ec2fac
16046469 <jie1n31997>
2018-05-07 14:09:37 +0800

    second activity info button

--- app/src/main/java/sg/edu/rp/c346/p03_classjournal/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p03_classjournal/SecondActivity.java
@@ -1,14 +1,17 @@
 package sg.edu.rp.c346.p03_classjournal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
-
+    Button btnInfo;
     ListView lvCAG;
 
     @Override
@@ -25,7 +28,17 @@ public class SecondActivity extends AppCompatActivity {
         Toast.makeText(SecondActivity.this, option+"",
                 Toast.LENGTH_LONG).show();
 
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
 
 
 
