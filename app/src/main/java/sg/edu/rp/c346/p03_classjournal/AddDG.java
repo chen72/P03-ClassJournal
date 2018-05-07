9eb7268343e417704669395ff1e34d53a0dc791e
LeoTian0000 <zhangzhongtian1@gmail.com>
2018-05-07 14:16:10 +0800

    From Leo

--- app/src/main/java/sg/edu/rp/c346/p03_classjournal/AddDG.java
+++ app/src/main/java/sg/edu/rp/c346/p03_classjournal/AddDG.java
@@ -2,12 +2,28 @@ package sg.edu.rp.c346.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AddDG extends AppCompatActivity {
-
+    TextView tvWeek;
+    RadioGroup radioGroup;
+    Button btnSubmit;
+    ArrayList<WeekCag> cag;
+    int nextWeek;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dg);
+
+        tvWeek = (TextView) findViewById(R.id.tvWeek);
+        radioGroup = findViewById(R.id.radioGroup);
+        btnSubmit = findViewById(R.id.submitBtn);
+
+
+
     }
 }
--- app/src/main/java/sg/edu/rp/c346/p03_classjournal/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p03_classjournal/SecondActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class SecondActivity extends AppCompatActivity {
 
 
     ListView lvCAG;
-
+    ArrayList<WeekCag> cag;
+    int nextWeek;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +23,17 @@ public class SecondActivity extends AppCompatActivity {
         // Get the intent
         Intent i = getIntent();
         // Get the Hero object first activity put in Intent
-        int option = i.getIntExtra("module",0);
+        int option = i.getIntExtra("module",-1);
 
         Toast.makeText(SecondActivity.this, option+"",
                 Toast.LENGTH_LONG).show();
 
+        cag = new ArrayList<WeekCag>();
+        cag.add(new WeekCag("1","B","null"));
+        cag.add(new WeekCag("2","C","null"));
+        cag.add(new WeekCag("3","A","null"));
+
+        nextWeek = cag.size() + 1;
 
 
 
